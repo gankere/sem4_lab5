cc61c4c62eccb3029edb07dbc7cc110a3d7666be
Anna Vladimirskaya <annav7822@gmail.com>
2026-06-06 07:44:07 +0700

    Remove .vscode and build folders, add .gitignore

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_painter_lab_FILE C:/Users/gankere/TPU_Workspace/painter_lab/build/painter_lab.exe)
-set(__QT_DEPLOY_TARGET_painter_lab_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_painter_lab_RUNTIME_DLLS C:/msys64/mingw64/bin/Qt6Widgets.dll;C:/msys64/mingw64/bin/Qt6Gui.dll;C:/msys64/mingw64/bin/Qt6Core.dll)
